3d550d6a88fdd75a40a6eec38ba573a3dc1c2339
Немтырёва Ксения <k.nemtyryova@gmail.com>
2022-09-26 12:25:43 +0300

    Добавьте файлы проекта.

---
+++ ClassSquareSolve.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryExample
+{
+    public class ClassSquareSolve
+    {
+        public string x1(int a, int b, int c)
+        {
+            double D;
+            D = b * b - 4 * a * c;
+            if(D >= 0)
+                return Math.Round((-b + Math.Sqrt(D))/(2*a),2).ToString();
+            return "Действительных корней нет";
+        }
+        public string x2(int a, int b, int c)
+        {
+            double D;
+            D = b * b - 4 * a * c;
+            if (D >= 0)
+                return Math.Round((-b - Math.Sqrt(D)) / (2 * a), 2).ToString();
+            return "Действительных корней нет";
+        }
+    }
+}
